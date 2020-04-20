61ebf0aacf1e43ca3507b095ad14c0650bfd88b2
Cristopher Ortega <ortega.cpp@gmail.com>
2020-04-20 17:16:27 -0400

    Validation: sign up parser properly tested

--- src/parser/signup.test.js
+++ src/parser/signup.test.js
@@ -1,18 +1,23 @@
 const parseSignup = require('./signup')
 
+const discordId = '474405294307278880'
+
 describe('signup parameters parser 📝', () => {
   test('It errors on empty strings', () =>
-    expect(() => parseSignup('')).toThrow(/empty/))
+    expect(() => parseSignup('', discordId)).toThrow(/empty/))
 
   test('It throws if no arguments are provided', () =>
-    expect(() => parseSignup('!ac signup')).toThrow(/arguments/))
+    expect(() => parseSignup('!ac signup', discordId)).toThrow(/arguments/))
 
   test('It throws if not enough arguments are provided', () =>
-    expect(() => parseSignup('!ac signup foo')).toThrow(/enough/))
+    expect(() => parseSignup('!ac signup foo', discordId)).toThrow(/enough/))
+  
+  test('It throws if it receives undefined as discordId', () =>
+    expect(() => parseSignup('!ac signup foo', undefined)).toThrow(/gathered/))
 
   test('It properly parses and returns the parameters for signup', () =>
-    expect(parseSignup('!ac signup foo github/foo')).toEqual([
+    expect(parseSignup('!ac signup foo github/foo', discordId)).toEqual([
       'foo',
-      ['github/foo', 'discord/undefined'],
+      ['github/foo', `discord/${discordId}`],
     ]))
 })
