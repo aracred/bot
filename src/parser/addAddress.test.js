edb932c1b5da6a0a7e110f693b6f5a7c6cf08cfe
YoungKidWarrior <ginelliunit@gmail.com>
2020-09-30 23:18:18 -0700

    remove signup

--- src/parser/addAddress.test.js
+++ src/parser/addAddress.test.js
@@ -8,12 +8,12 @@ describe('addAddress handler tests', () => {
     expect(() => parseAddAddress('')).toThrow(/empty/))
 
   test('It should error if not enough arguments are provided', () =>
-    expect(() => parseAddAddress('!ac addaddress')).toThrow(/arguments/))
+    expect(() => parseAddAddress('!she addaddress')).toThrow(/arguments/))
 
   test('It should properly pass a well formed message', () =>
     expect(
       parseAddAddress(
-        '!ac addaddress 0x960b236A07cf122663c4303350609A66A7B288C0',
+        '!she addaddress 0x960b236A07cf122663c4303350609A66A7B288C0',
       ),
     ).toEqual('0x960b236A07cf122663c4303350609A66A7B288C0'))
 })
