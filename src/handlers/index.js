1244bb6144e6a4dd631d241894dbc3cb5800a8dc
YoungKidWarrior <ginelliunit@gmail.com>
2020-10-02 01:25:07 -0700

    token command

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,9 +1,11 @@
 const addAddressHandler = require('./addAddress')
 const helpHandler = require('./help')
+const tokenHandler = require('./token')
 
 const handlers = new Map([
   ['addaddress', addAddressHandler],
   ['help', helpHandler],
+  ['token', tokenHandler]
 ])
 
 module.exports = handlers
---
+++ src/handlers/token.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js')
+const fetch = require('node-fetch')
+
+module.exports = function token(message) {
+  const query = `{
+    token(id:"0xb5d592f85ab2d955c25720ebe6ff8d4d1e1be300") {
+      totalLiquidity
+      name
+      symbol
+      tradeVolumeUSD
+    }
+  }`
+
+  fetch('https://api.thegraph.com/subgraphs/name/1hive/uniswap-v2', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+    }),
+  })
+    .then(r => r.json())
+    .then(({ data: { token } }) => {
+      const embed = new Discord.MessageEmbed()
+        .setColor('#ff006c')
+        .setTitle(`$${token.symbol} Token 🤸‍♀️`)
+        .setURL('https://she.energy/swap')
+        .setDescription(`Token Address: ${token.address}`)
+        .setThumbnail(
+          'https://media.discordapp.net/attachments/708186850359246859/760681364163919994/1601430947224.png',
+        )
+        .setTimestamp()
+        .setFooter('Bot made by the Shenanigan team')
+      message.send(embed)
+    })
+}
